6efd07ed5d609300a25b16440b7acdc9baf60453
siddiq24 <siddiqrm24@gmail.com>
2025-11-03 17:04:46 +0700

    fix: fixing typo

--- down/000005_create_table_orders.down.sql
+++ down/000005_create_table_orders.down.sql
@@ -1,7 +1,7 @@
 ALTER TABLE orders_products
 DROP COLUMN order_id;
 DROP TABLE payment_info;
-DROP TABLE payment_method;
+DROP TABLE payment_methods;
 DROP TABLE orders;
 DROP TABLE status;
 DROP TABLE shippings;
